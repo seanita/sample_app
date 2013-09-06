b8707fda831df2253b4a181460a6f15bee724253
Seanita Tolliver <seanitat@gmail.com>
2013-09-06 13:21:28 -0400

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy  
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -6,7 +14,26 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def edit
+  end
   
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end   
+
   def create
   	@user = User.new(user_params)
   	if @user.save
@@ -14,6 +41,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
+      flash.now[:error] = 'Invalid email/password combination'
   		render 'new'
   	end
   end
@@ -21,5 +49,23 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
